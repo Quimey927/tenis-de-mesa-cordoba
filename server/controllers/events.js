0e49b4304ffe6cb474ccae717449892d44e7f929
Quimey <quimeymata@gmail.com>
2023-04-30 00:45:17 -0300

    Adds id to dummy events returned from the backend

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -4,14 +4,22 @@ module.exports.getCurrentEvents = async (req, res) => {
       {
         title: 'Provinciales',
         season: 'Temporada 2023',
+        id: 1,
       },
       {
         title: 'Liga de Equipos',
         season: 'Temporada 2023',
+        id: 2,
       },
       {
         title: 'Torneo +30 Hermau',
         season: 'Primer Semestre 2023',
+        id: 3,
+      },
+      {
+        title: 'Torneo UGAB',
+        season: '2023',
+        id: 4,
       },
     ];
 
