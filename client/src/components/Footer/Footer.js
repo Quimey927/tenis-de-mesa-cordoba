b6abb8dcb7caff94c747b270c5ca2633b6dc10ee
Quimey <quimeymata@gmail.com>
2023-04-29 22:07:46 -0300

    Adds footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,12 @@
+import classes from './Footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={classes.footer}>
+      <span>Torneos Tenis de Mesa Córdoba</span>
+      <span>2023</span>
+    </footer>
+  );
 };
 
 export default Footer;
