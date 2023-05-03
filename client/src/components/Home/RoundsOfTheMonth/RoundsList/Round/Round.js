2eb7e4c3fc9ea5878581efb334e555eabdb1d3b6
Quimey <quimeymata@gmail.com>
2023-05-03 06:49:00 -0300

    Adds Round component with its more elegant styles

--- client/src/components/Home/RoundsOfTheMonth/RoundsList/Round/Round.js
+++ client/src/components/Home/RoundsOfTheMonth/RoundsList/Round/Round.js
@@ -1,39 +1,60 @@
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
+import Button from '../../../../UI/Button/Button';
+import { months } from '../../../../../services/constants/months';
 import classes from './Round.module.css';
 
 const Round = ({ round }) => {
-  const weekDay =
-    round.weekDays.length === 1
-      ? round.weekDays[0]
-      : `${round.weekDays[0].substring(0, 3)}/${round.weekDays[1].substring(
-          0,
-          3
-        )}`;
+  const [, month, startDay] = round.startDate.split('-');
+  const [, , finishDay] = round.finishDate.split('-');
 
-  const days =
-    round.days.length === 1
-      ? round.days[0]
-      : `${round.days[0]} y ${round.days[1]}`;
+  const roundDays =
+    finishDay !== startDay ? (
+      <p>
+        {+startDay} <span>y</span> {+finishDay}
+      </p>
+    ) : (
+      <p>{+startDay}</p>
+    );
+
+  const encodedTournament = round.tournament.toLowerCase().split(' ').join('-');
+  const encodedSeason = round.season.toLowerCase().split(' ').join('-');
+  const encodedTitle = round.title.toLowerCase().split(' ').join('-');
+
+  const encodedUrl = `/${encodedTournament}/${encodedSeason}/${encodedTitle}`;
 
   return (
-    <li className={classes.round}>
-      <div className={classes.bla}>
-        <div className={classes['round__date']}>
-          <span className={classes['week-day']}>{weekDay}</span>
-          <span className={classes.days}>{days}</span>
+    <div className={classes['round']}>
+      <div className={classes['round__wrapper']}>
+        <div className={`${classes['calendar']} calendar`}>
+          <span className={classes['calendar__month']}>
+            {months[month - 1]}
+          </span>
+          <div className={classes['calendar__days']}>{roundDays}</div>
         </div>
-        <div className={classes['round__description']}>
-          <p>{round.tournament}</p>
-          <p>{round.title}</p>
+        <div className={classes['description']}>
+          <p className={classes['description__tournament']}>
+            {round.tournament}
+          </p>
+          <p className={classes['description__title']}>{round.title}</p>
+          <Button className={classes.btn}>
+            <Link to={encodedUrl}>Ver fecha</Link>
+          </Button>
         </div>
       </div>
-      <div className={classes['round__location']}>
-        <FontAwesomeIcon icon={faLocationDot} color="white" />
-        {round.location}
+      <div className={classes.location}>
+        <div className={classes['location__info']}>
+          <p>{round.location}</p>
+          <p>{round.address}</p>
+        </div>
+        <FontAwesomeIcon
+          icon={faLocationDot}
+          className={classes['location__icon']}
+        />
       </div>
-    </li>
+    </div>
   );
 };
 
