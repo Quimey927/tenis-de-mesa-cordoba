0b09cb4da8429bf4117ecc5595d1ccd9160c001a
Quimey <quimeymata@gmail.com>
2023-05-28 17:11:18 -0300

    Adds uppercase to last names on getFullNames

--- client/src/services/utils/getFullName.js
+++ client/src/services/utils/getFullName.js
@@ -5,6 +5,8 @@ export const getFullName = (
   second_last_name
 ) => {
   return `${
-    second_last_name ? `${last_name} ${second_last_name},` : `${last_name},`
+    second_last_name
+      ? `${last_name.toUpperCase()} ${second_last_name.toUpperCase()},`
+      : `${last_name.toUpperCase()},`
   } ${middle_name ? `${first_name} ${middle_name}` : first_name}`;
 };
