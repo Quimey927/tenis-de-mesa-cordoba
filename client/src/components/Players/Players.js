bd32a86f0df5e905e5adcb890a67f185e2fcb243
Quimey <quimeymata@gmail.com>
2023-05-28 17:09:46 -0300

    Adds key to players list rendering

--- client/src/components/Players/Players.js
+++ client/src/components/Players/Players.js
@@ -5,7 +5,7 @@ const Players = ({ players }) => {
   return (
     <ul className={classes['players-list']}>
       {players.map((player) => (
-        <Player player={player} />
+        <Player key={player.id} player={player} />
       ))}
     </ul>
   );
