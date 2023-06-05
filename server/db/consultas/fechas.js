42b1d3a8801bb63710d603f6252fa5ec58836206
Quimey <quimeymata@gmail.com>
2023-06-04 23:12:07 -0300

    Adds id to obtenerFechasDelMes to key attribute

--- server/db/consultas/fechas.js
+++ server/db/consultas/fechas.js
@@ -2,7 +2,7 @@ const obtenerFechas =
   'SELECT f.id AS id, f.nombre AS nombre, t.titulo AS torneo, t.temporada AS temporada, f.id_torneo AS id_torneo, t.slug AS slug_torneo FROM fechas AS f INNER JOIN torneos AS t ON t.id = f.id_torneo ORDER BY f.fecha_inicio DESC';
 
 const obtenerFechasDelMes =
-  'SELECT f.nombre AS nombre_fecha, t.titulo AS titulo_torneo, f.fecha_inicio AS fecha_inicio, f.fecha_finalizacion AS fecha_finalizacion, c.nombre AS club, t.imagen_torneo AS imagen_torneo, c.direccion AS direccion, f.slug AS slug FROM fechas as f INNER JOIN torneos AS t ON t.id = f.id_torneo INNER JOIN clubes AS c ON c.id = f.id_club WHERE (f.fecha_inicio >= $1 AND f.fecha_inicio < $2) OR (f.fecha_finalizacion >= $1 AND f.fecha_finalizacion < $2) ORDER BY f.fecha_inicio';
+  'SELECT f.id AS id, f.nombre AS nombre_fecha, t.titulo AS titulo_torneo, f.fecha_inicio AS fecha_inicio, f.fecha_finalizacion AS fecha_finalizacion, c.nombre AS club, t.imagen_torneo AS imagen_torneo, c.direccion AS direccion, f.slug AS slug FROM fechas as f INNER JOIN torneos AS t ON t.id = f.id_torneo INNER JOIN clubes AS c ON c.id = f.id_club WHERE (f.fecha_inicio >= $1 AND f.fecha_inicio < $2) OR (f.fecha_finalizacion >= $1 AND f.fecha_finalizacion < $2) ORDER BY f.fecha_inicio';
 
 const crearFecha =
   'INSERT INTO fechas (nombre, num_fecha, id_torneo, id_club, fecha_inicio, fecha_finalizacion, slug) VALUES ($1, $2, $3, $4, $5, $6, $7)';
