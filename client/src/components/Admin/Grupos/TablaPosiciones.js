84098df65a361f40e1a3b4ceadcafcfaf1c2fab1
Quimey <quimeymata@gmail.com>
2023-06-20 20:46:23 -0300

    Moves datosTabla to a separate constant file

--- client/src/components/Admin/Grupos/TablaPosiciones.js
+++ client/src/components/Admin/Grupos/TablaPosiciones.js
@@ -8,6 +8,7 @@ import {
 } from '../../../utils/funcionesColorFila';
 import { editarFilaTabla } from '../../../api';
 import { obtenerNombreCompleto } from '../../../utils/obtenerNombreCompleto';
+import { datosTabla } from '../../../constants/datosTabla';
 import classes from './TablaPosiciones.module.css';
 
 const TablaPosiciones = ({ filasTabla, coloresTabla, setDummyEstado }) => {
@@ -43,37 +44,6 @@ const TablaPosiciones = ({ filasTabla, coloresTabla, setDummyEstado }) => {
   }, [idFilaEditandose, filasTabla]);
 
   const coloresFila = obtenerColoresFila(coloresTabla);
-
-  const datosTabla = [
-    {
-      dato: 'Partidos Jugados',
-      codigo: 'pj',
-    },
-    {
-      dato: 'Partidos Ganados',
-      codigo: 'pg',
-    },
-    {
-      dato: 'Partidos Perdidos',
-      codigo: 'pp',
-    },
-    {
-      dato: 'Sets a Favor',
-      codigo: 'sf',
-    },
-    {
-      dato: 'Sets en Contra',
-      codigo: 'sc',
-    },
-    {
-      dato: 'Puntos a Favor',
-      codigo: 'pf',
-    },
-    {
-      dato: 'Puntos en Contra',
-      codigo: 'pc',
-    },
-  ];
 
   return (
     <>
---
+++ client/src/constants/datosTabla.js
@@ -0,0 +1,30 @@
+export const datosTabla = [
+  {
+    dato: 'Partidos Jugados',
+    codigo: 'pj',
+  },
+  {
+    dato: 'Partidos Ganados',
+    codigo: 'pg',
+  },
+  {
+    dato: 'Partidos Perdidos',
+    codigo: 'pp',
+  },
+  {
+    dato: 'Sets a Favor',
+    codigo: 'sf',
+  },
+  {
+    dato: 'Sets en Contra',
+    codigo: 'sc',
+  },
+  {
+    dato: 'Puntos a Favor',
+    codigo: 'pf',
+  },
+  {
+    dato: 'Puntos en Contra',
+    codigo: 'pc',
+  },
+];
