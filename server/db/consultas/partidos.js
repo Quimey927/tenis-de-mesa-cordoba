546fbfe5c71dcf7ed7e9d06d901fe2bbc0dff17c
Quimey <quimeymata@gmail.com>
2023-06-23 16:09:02 -0300

    Adds sum of points to obtenerPartidosDelGrupos

--- server/db/consultas/partidos.js
+++ server/db/consultas/partidos.js
@@ -7,7 +7,9 @@ const obtenerPartidosDelGrupo = `SELECT
     j2.nombre AS jugador_2_nombre,
     j2.apellido AS jugador_2_apellido,
     j2.segundo_nombre AS jugador_2_segundo_nombre,
-    j2.segundo_apellido AS jugador_2_segundo_apellido
+    j2.segundo_apellido AS jugador_2_segundo_apellido,
+    SUM(set.puntos_jugador_1) AS puntos_jugador_1,
+    SUM(set.puntos_jugador_2) AS puntos_jugador_2
   FROM partidos AS part
   LEFT JOIN sets AS set ON set.id_partido = part.id
   INNER JOIN jugadores AS j1 ON j1.id = part.id_jugador_1
