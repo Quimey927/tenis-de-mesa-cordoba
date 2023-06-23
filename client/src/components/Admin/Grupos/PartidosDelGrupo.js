f4191b2ee05bdf427003462f70dc09479cec8325
Quimey <quimeymata@gmail.com>
2023-06-23 17:27:13 -0300

    Adds logic to recaulculate tables

--- client/src/components/Admin/Grupos/PartidosDelGrupo.js
+++ client/src/components/Admin/Grupos/PartidosDelGrupo.js
@@ -15,6 +15,7 @@ import {
   editarSet,
   crearSet,
   borrarSet,
+  editarFilaTabla,
 } from '../../../api';
 import { obtenerNombreCompleto } from '../../../utils/obtenerNombreCompleto';
 import classes from './PartidosDelGrupo.module.css';
@@ -129,8 +130,6 @@ const PartidosDelGrupo = ({
   };
 
   const controladorTerminarEdicionSets = () => {
-    // console.log(idPartidoSetsEditandose);
-    console.log(partidosDelGrupo);
     let sets_jugador_1 = 0;
     let sets_jugador_2 = 0;
     for (let set of setsPartido) {
@@ -146,7 +145,63 @@ const PartidosDelGrupo = ({
 
     const nuevasFilasTabla = [...filasTabla];
 
-    // operar con nuevas filas tabla
+    for (let fila of nuevasFilasTabla) {
+      fila.pj = 0;
+      fila.pg = 0;
+      fila.pp = 0;
+      fila.sf = 0;
+      fila.sc = 0;
+      fila.pf = 0;
+      fila.pc = 0;
+
+      for (let partido of partidosDelGrupo) {
+        if (fila.id_jugador === partido.id_jugador_1) {
+          fila.pj++;
+          fila.sf += partido.sets_jugador_1;
+          fila.sc += partido.sets_jugador_2;
+          partido.sets_jugador_1 > partido.sets_jugador_2
+            ? fila.pg++
+            : fila.pp++;
+          fila.pf += +partido.puntos_jugador_1;
+          fila.pc += +partido.puntos_jugador_2;
+        }
+
+        if (fila.id_jugador === partido.id_jugador_2) {
+          fila.pj++;
+          fila.sf += partido.sets_jugador_2;
+          fila.sc += partido.sets_jugador_1;
+          partido.sets_jugador_2 > partido.sets_jugador_1
+            ? fila.pg++
+            : fila.pp++;
+          fila.pf += +partido.puntos_jugador_2;
+          fila.pc += +partido.puntos_jugador_1;
+        }
+      }
+    }
+
+    nuevasFilasTabla.sort((a, b) => {
+      if (a.pg === b.pg) {
+        const a_cs = a.sf / a.sc;
+        const b_cs = b.sf / b.sc;
+        if (a_cs === b_cs) {
+          const a_cp = a.pf / a.pc;
+          const b_cp = b.pf / b.pc;
+          return a_cp > b_cp ? -1 : 1;
+        } else {
+          return a_cs > b_cs ? -1 : 1;
+        }
+      } else {
+        return a.pg > b.pg ? -1 : 1;
+      }
+    });
+
+    // el .entries() retorna un nuevo array con los pares [index, valorDelArray], y con ES6 destructuramos
+    for (let [i, fila] of nuevasFilasTabla.entries()) {
+      fila.posicion = i + 1;
+      console.log(nuevasFilasTabla[i]);
+      console.log(fila);
+      editarFilaTabla(fila);
+    }
 
     setIdPartidoSetsEditandose(null);
     setIdPartidoEditandose(null);
