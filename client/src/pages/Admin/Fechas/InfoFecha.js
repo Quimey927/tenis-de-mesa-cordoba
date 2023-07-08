ee667eb3c5d1f42307f5427999fe4a2c374f1ad8
Quimey <quimeymata@gmail.com>
2023-07-08 14:39:12 -0300

    Fixes go back from a fecha to the corresp. torneo

--- client/src/pages/Admin/Fechas/InfoFecha.js
+++ client/src/pages/Admin/Fechas/InfoFecha.js
@@ -47,6 +47,7 @@ const PaginaInfoFecha = () => {
         subtitulo={`${fecha[0].torneo} ${fecha[0].temporada}`}
         campos={campos}
         imagen={fecha[0].imagen}
+        to={`/admin/torneos/${fecha[0].id_torneo}`}
       />
 
       <ListaCategoriasFecha
